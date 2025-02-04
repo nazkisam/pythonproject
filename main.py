24ec76b4c2720002eb365e39c521573ab5a8e9ce
Sameem <nsam603@gmail.com>
2025-02-04 20:55:06 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def deposit():
-  while True:
-   amount = input('what would you like to deposit? $')
-   if amount.isdigit():
-     amount = int(amount)
-     if amount > 0:
-        break
-     else:
-       print('amount must be positive')
-      
+    while True:
+        amount = input('What would you like to deposit? $')
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                print(f"Deposit successful: ${amount}")  # Debugging line
+                return amount
+            else:
+                print('Amount must be positive.')
+        else:
+            print('Please enter a number.')
+
+# Call the function
+deposit()
