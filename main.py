d6a2ebc555bfa27249fe6b000e0fa0d7b49b0129
Sameem <nsam603@gmail.com>
2025-02-05 14:15:07 +0530

    creating a slot machine from scratch

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#global constant
+MAX_LINES = 3
+
+
+
 def deposit():
     while True:
         amount = input('What would you like to deposit? $')
@@ -7,9 +12,31 @@ def deposit():
                 print(f"Deposit successful: ${amount}")  # Debugging line
                 return amount
             else:
-                print('Amount must be positive.')
+                print('Amount must be greater than 0.')
         else:
-            print('Please enter a number.')
+            print('Please enter a positive number.')
 
 # Call the function
-deposit()
+
+
+
+def pick_number_of_lines():
+       while True:
+        lines = input('enter the number of lines you want to bet on (1-'+str(MAX_LINES)+')?')
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<=lines<=MAX_LINES:
+                break
+            else:
+                print('enter a valid number of lines.')
+        else:
+            print('enter a number')
+       return lines
+
+
+def main():
+ balance = deposit()
+ lines  = pick_number_of_lines()
+ print(balance,lines)
+ 
+main()
